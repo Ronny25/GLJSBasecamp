15ec054227027f9bc3fbddb20ad8df6dbe641f8a
Dima Kotkin <kotkin.lg@gmail.com>
2016-07-10 18:44:19 +0300

    first commit

---
+++ lesson4_oop/js/script.js
@@ -0,0 +1,42 @@
+/*
+21.    Implement the following:
+
+1.    Define a Shape constructor. Objects created with Shape should have a `type` property and a `getType` method.
+2.    Define a Triangle constructor. Objects created with Triangle should inherit from Shape and have three own properties:
+a, b and c representing the sides of a triangle.
+3.    Add a new method to the prototype called `getPerimeter`.
+
+Test your implementation with this code:
+
+var t = new Triangle(1, 2, 3);
+t.constructor;                 // Triangle(a, b, c)
+t instanceof Shape                // true
+t.getPerimeter();              // 6
+t.getType();                   // "triangle"
+*/
+function Shape() {
+  this.type = 'triangle',
+
+  this.getType = function() {
+    return this.type;
+  }
+}
+
+function Triangle( a, b, c ) {
+  this.a = a,
+  this.b = b,
+  this.c = c
+}
+
+Triangle.prototype = Object.create(Shape.prototype);
+Triangle.prototype.constructor = Triangle;
+Triangle.prototype.getPerimeter = function() {
+  return this.a + this.b + this.c;
+}
+
+var triangle = new Triangle( 1, 2, 3 );
+
+console.log( triangle.constructor );
+console.log( triangle instanceof Shape );
+console.log( triangle.getPerimeter() );
+console.log( triangle.getType() );
