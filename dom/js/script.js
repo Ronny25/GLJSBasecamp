7033e83337a7f23bb8dec7bf347e758698d75aa9
Dima Kotkin <kotkin.lg@gmail.com>
2016-07-14 21:32:17 +0300

    done

--- dom/js/script.js
+++ dom/js/script.js
@@ -78,7 +78,66 @@ insertAfter( '#line-0', newElem );
     	    <chucknorris speed="Infinity"></chucknorris>
 */
 function attributing( attr, val ) {
-  var el = document.querySelector('#sel');
-  console.log(el.hasAttribute(attr));
+  var el = document.querySelector('#line-1');
+
+  if (el.getAttribute(attr) !== null) {
+    return console.log(el.getAttribute(attr));
+  }
+
+  if (val) {
+    el.style.setProperty( attr, val );
+    return console.log(el);
+  }
+
+  return console.log('атрибут не задан');
 }
-attributing('color');
+attributing('background', 'red');
+
+/*
+4. С помощью JS создайте шахматное поле:
+    - контейнер поля
+    - 64 ребёнка (ячейки) элементы (проще позиционировать с помощью float)
+    - ячейки раскрашены белым и черным
+    - нужные аттрибуты и стили задавайте с помощью JS
+*/
+var container = document.createElement('div');
+container.classList.add('container');
+container.style.setProperty('margin', 'auto');
+container.style.setProperty('margin-bottom', '100px');
+container.style.setProperty('width', '800px');
+container.style.setProperty('border', '10px solid #fff');
+container.style.setProperty('box-shadow', '5px 5px 3px 50px');
+
+for (var i = 1; i < 65; i++) {
+  var squares = container.appendChild(document.createElement('div'));
+  squares.classList.add('square');
+  squares.style.setProperty('display', 'inline-block');
+  squares.style.setProperty('margin-top', '-4px');
+}
+
+document.body.appendChild(container);
+
+var all = document.querySelectorAll('.square');
+for (var i = 0; i < all.length; i++) {
+  all[i].style.setProperty('width', '100px');
+  all[i].style.setProperty('height', '100px');
+}
+for (var i = 0; i < 9; i++) {
+  all[i].style.setProperty('margin', '0');
+}
+for (var i = 9; i < 17; i++) {
+  all[i].style.setProperty('float', 'left');
+}
+for (var i = 25; i < 33; i++) {
+  all[i].style.setProperty('float', 'left');
+}
+for (var i = 41; i < 49; i++) {
+  all[i].style.setProperty('float', 'left');
+}
+for (var i = 57; i < 64; i++) {
+  all[i].style.setProperty('float', 'left');
+}
+for (var i = 1; i < all.length; i += 2) {
+  all[i].classList.add('black');
+  all[i].style.setProperty('background', 'black');
+}
