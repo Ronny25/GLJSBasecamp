2307fabe1667c1bb38ae706504cc39eb07d5229b
Dima Kotkin <kotkin.lg@gmail.com>
2016-07-19 12:21:33 +0300

    optimized

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -8,25 +8,13 @@ function letsDance( url, bookUrl, bookId, authorUrl, authorId, similarBookUrl, s
   author.textContent = 'Please wait. Author details are loading';
   similarBook.textContent = 'Please wait. Similar books are loading';
 
-  function status(response) {
-    if (response.status >= 200 && response.status < 300) {
-      return Promise.resolve(response)
-    }
-    return Promise.reject(new Error(response.statusText))
-  }
-
   fetch(url + bookUrl + bookId)
-    .then(status)
     .then(function(data) { //get book title
       bookElem.textContent = data.name;
       return fetch(url + authorUrl + authorId);
     })
     .then(function(data) {
       authorElem.textContent = data.name;
-
-      var similarBooksLoaded = 0;
-      var similarBooksAmount = data.books.lenght;
-
       return Promise.all(
         data.books.map(function(similarBookId) {
           return fetch(url + similarBookUrl + similarBookId);
@@ -37,11 +25,9 @@ function letsDance( url, bookUrl, bookId, authorUrl, authorId, similarBookUrl, s
       similarities.forEach(function (similarBookId) {
         var p = document.createElement('p');
         similarBookElem.appendChild(p.textContent = similarBookId);
-
-        similarBooksLoaded += 1;
       });
 
-      if(similarBooksLoaded === similarBooksAmount) {
+      if(similarities) {
         alert('Horray everything loaded');
       }
     })
